99bae956d94e443e65a363b7b7127e60d44e3b49
ivart <ivar.the.door@gmail.com>
2025-01-16 09:22:52 +0100

    lifes added

--- game.py
+++ game.py
@@ -7,18 +7,22 @@ password = ["_" for i in range(lenght)]
 
 
 def print_data():
+    print("".join(lifes))
     print(f"użyte litery: {", ".join(wrong_letters)}")
-    print("".join(password))
+    print(f"nazwa: {"".join(password)}")
 
+print("".join(lifes))
+print(f"użyte litery: {", ".join(wrong_letters)}")
 print(f"nazwa: {"".join(password)}")
 
-while "_" in  password:
+while "_" in  password and len(lifes) >= 1:
     user_input = input("Podaj literę: ")
     if len(user_input) > 1:
         print("max 1 litera...")
         print_data()
     elif user_input in  wrong_letters:
         print("było...")
+        lifes.pop()
         print_data()
     elif not (user_input.isalpha() or user_input == " "):
         print("Tylko litery...")
@@ -31,5 +35,6 @@ while "_" in  password:
             print_data()
         elif user_input not in word:
             wrong_letters.append(user_input)
+            lifes.pop()
             print_data()
         
