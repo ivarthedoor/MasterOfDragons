5d598b0cc525029953762278a987e5e8b86a9971
ivart <ivar.the.door@gmail.com>
2025-01-20 18:45:29 +0100

    questions task moved to class

--- questions_task.py
+++ questions_task.py
@@ -1,62 +1,55 @@
 import json
 import random
-from system_functions import sleep_and_clear
+from system_functions import sleep_and_clear, read_data
 
-with open('questions.json', 'r', encoding='UTF-8') as file:
-    data = json.load(file)
+class QuestionsTask:
+    def __init__(self):
+        self.question = random.choice(read_data())
+        self.lenght = len(self.question["answer"])
+        self.lifes = ["\U0001F534", "\U0001F534", "\U0001F7E0", "\U0001F7E0", "\U0001F7E0", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2"]
+        self.used_letters = []
+        self.password = ["_" for i in range(self.lenght)]
 
+    def print_data(self):
+        sleep_and_clear(1)
+        print("".join(self.lifes))
+        print(self.question["description"])
+        print(f"Used letters: {", ".join(self.used_letters)}")
+        print(f"Answer: {"".join(self.password)}")
 
-question = random.choice(data)
-# word = "czarodziej"
-lenght = len(question["answer"])
-lifes = ["\U0001F534", "\U0001F534", "\U0001F7E0", "\U0001F7E0", "\U0001F7E0", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2", "\U0001F7E2"]
-wrong_letters = []
-password = ["_" for i in range(lenght)]
+    def task_loop(self):
+        self.print_data()
+        while "_" in  self.password and len(self.lifes) > 0:
+            user_input = input("Put your letter: ")
+            if len(user_input) > 1:
+                print("max 1 letter...")
+                self.print_data()
+            elif user_input in  self.used_letters:
+                print("You used that letter already...")
+                self.lifes.pop()
+                self.print_data()
+            elif not (user_input.isalpha() or user_input == " "):
+                print("Letters only...")
+                self.print_data()
+            else:
+                if user_input in self.question["answer"]:
+                    for x in range(self.lenght):
+                        if self.question["answer"][x] == user_input:
+                            self.password[x] = user_input
+                            self.used_letters.append(user_input)
+                    self.print_data()
+                elif user_input not in self.question["answer"]:
+                    self.used_letters.append(user_input)
+                    self.lifes.pop()
+                    self.print_data()
 
-
-
-def print_data():
-    sleep_and_clear(1)
-    print("".join(lifes))
-    print(question["description"])
-    print(f"użyte litery: {", ".join(wrong_letters)}")
-    print(f"nazwa: {"".join(password)}")
-
-print("".join(lifes))
-print(question["description"])
-print(f"użyte litery: {", ".join(wrong_letters)}")
-print(f"nazwa: {"".join(password)}")
-
-
-
-while "_" in  password and len(lifes) > 0:
-    user_input = input("Podaj literę: ")
-    if len(user_input) > 1:
-        print("max 1 litera...")
-        print_data()
-    elif user_input in  wrong_letters:
-        print("było...")
-        lifes.pop()
-        print_data()
-    elif not (user_input.isalpha() or user_input == " "):
-        print("Tylko litery...")
-        print_data()
-    else:
-        if user_input in question["answer"]:
-            for x in range(lenght):
-                if question["answer"][x] == user_input:
-                    password[x] = user_input
-            print_data()
-        elif user_input not in question["answer"]:
-            wrong_letters.append(user_input)
-            lifes.pop()
-            print_data()
-
-    if len(lifes) == 0:
-        print(f"Przegrałeś, hasło to {question["answer"]}")
-        sleep_and_clear(5)
-        break
-    if "_" not in password:
-        print("Zgadłeś hasło")
-        sleep_and_clear(5)
-   
+            if len(self.lifes) == 0:
+                print(f"You lost, answer is: {self.question["answer"]}")
+                sleep_and_clear(5)
+                break
+            if "_" not in self.password:
+                print("Correct!")
+                sleep_and_clear(5)
+        
+loop = QuestionsTask()
+loop.task_loop()
--- system_functions.py
+++ system_functions.py
@@ -1,5 +1,6 @@
 import os
 from time import sleep
+import json
 
 
 def clear():
@@ -11,3 +12,8 @@ def clear():
 def sleep_and_clear(time):
     sleep(time)
     clear()
+
+def read_data():
+    with open('questions.json', 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+    return data
