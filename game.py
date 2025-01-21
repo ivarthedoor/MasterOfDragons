479e3492fd94bbe6a53c39d8255b3424b93320c4
ivart <ivar.the.door@gmail.com>
2025-01-21 14:21:30 +0100

    some texts added

--- game.py
+++ game.py
@@ -7,25 +7,8 @@ class InitializeGame(PlayerData):
         self.player = PlayerData()
         self.choices = PlayersChoices()
 
-    def first_dialog(self):
-        if self.choices.first_dialog_choice == "Wisdom":
-            print("You seek wisdom... A wise decision, young one.\nDifficult path lays ahead, first you must prove, you are worthy of my knowledge.")
-            sleep_and_clear(10)
-            print("To prove that, you will have to overcome all challenges I will put on you.")
-            sleep_and_clear(10)
-        elif self.choices.first_dialog_choice == "Power":
-            print("You seek power... Many have fallen to that temptation.\nYet only few gained enough power to reach the end of the path.")
-            sleep_and_clear(10)
-            print("We shall see if you are worthy of power of a dragon!")
-            sleep_and_clear(10)
-        elif self.choices.first_dialog_choice == "Kill a dragon":
-            print("You wish to destroy... There is a dark path ahead.\nAs dark as your soul might mecome if you will continue this path.\nThink wisely, hard task is before you... harder than you can imagine...")
-            sleep_and_clear(10)
-            print("If you wish to kill me, you will have to reach me first Hahahahahaha!")
-            sleep_and_clear(10)
-
     def run_the_game(self):
-        print(f"    Welcome Wanderer...\n \
+        print(f"    Ancalacan: Welcome Wanderer...\n \
     You shall fear, for I am a ghost of Ancalacan, fear of the east...\n \
     The greatest of all dragons...\n \
     You dare to attempt to my temple!\n \
@@ -33,13 +16,22 @@ class InitializeGame(PlayerData):
         sleep_and_clear(1)
         self.player.initialize_player()
         sleep_and_clear(1)
-        print(f"Welcome {self.player.nick}, the {self.player.type}\nwhat do you seek in my home...")
+        print(f"Ancalacan: Welcome {self.player.nick}, the {self.player.type}\nwhat do you seek in my home...")
         self.choices.initialize_first_choice()
-        self.first_dialog()
-
+        print("You enter through ancient stone door, to an old, ruined hall.\n \
+              walls and roof are full of holes, aeverything is covered in moss. You feel the smell of water and rot.\n \
+              You make few steps inside and right after you enter the room, something hits you from the back!")
+        sleep_and_clear(20)
+        print("...")
+        sleep_and_clear(10)
+        print("Goblin: ghrghnhthnhmrht")
+        sleep_and_clear(5)
+        print("You wake up, beeing dragged by leg, by stinking, ugly, little creature.\n \
+              You use other leg to kick goblin in the back!\n \
+              Right after hit, he drops your leg and falls.\n \
+              In ")
 
         
 
-# Tworzenie instancji i uruchamianie gry
 run = InitializeGame()
 run.run_the_game()
--- players_choices.py
+++ players_choices.py
@@ -19,5 +19,22 @@ class PlayersChoices:
 
     def initialize_first_choice(self):
         self.first_dialog_choice = self.first_d_choice[self.get_choice("1. I seek for wisdom\n2. I seek for power\n3. I'm here to destroy you\n", self.first_d_choice.keys())]
-
+        if self.first_dialog_choice == "Wisdom":
+            sleep_and_clear(0.1)
+            print("Ancalacan: You seek wisdom... A wise decision, young one.\nDifficult path lays ahead, first you must prove, you are worthy of my knowledge.")
+            sleep_and_clear(10)
+            print("Ancalacan: To prove that, you will have to overcome all challenges I will put on you.")
+            sleep_and_clear(10)
+        elif self.first_dialog_choice == "Power":
+            sleep_and_clear(0.1)
+            print("Ancalacan: You seek power... Many have fallen to that temptation.\nYet only few gained enough power to reach the end of the path.")
+            sleep_and_clear(10)
+            print("Ancalacan: We shall see if you are worthy of power of a dragon!")
+            sleep_and_clear(10)
+        elif self.first_dialog_choice == "Kill a dragon":
+            sleep_and_clear(0.1)
+            print("Ancalacan: You wish to destroy... There is a dark path ahead.\nAs dark as your soul might mecome if you will continue this path.\nThink wisely, hard task is before you... harder than you can imagine...")
+            sleep_and_clear(10)
+            print("Ancalacan: If you wish to kill me, you will have to reach me first Hahahahahaha!")
+            sleep_and_clear(10)
 
